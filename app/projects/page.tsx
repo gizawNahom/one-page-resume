4c702ffe85661b9ef4de3a8a9166f6296816266c
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-10-11 21:02:44 +0300

    fix: fix build error

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -74,9 +74,9 @@ export function Project({
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="project__link-icon"
         >
           <title>External Link</title>
@@ -97,9 +97,9 @@ export function Project({
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="project__link-icon"
         >
           <title>GitHub</title>
