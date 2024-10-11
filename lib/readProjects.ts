a0d2996b8507379ee9d5f7d47b80b1cb4284700c
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-10-11 16:41:15 +0300

    new: create project reading function

---
+++ lib/readProjects.ts
@@ -0,0 +1,44 @@
+import fs from "fs/promises";
+import path from "path";
+import matter from "front-matter";
+import { marked } from "marked";
+
+interface Project {
+  name: string;
+  description: string;
+  tools: string[];
+  //   github?: string;
+  //   external?: string;
+  //   cover: string;
+}
+
+export async function readProjects(directory: string): Promise<Project[]> {
+  const markdownFiles = await readProjectFiles();
+  return await parseProjects();
+
+  async function readProjectFiles() {
+    const files = await fs.readdir(directory);
+    const markdownFiles = files.filter((file) => file.endsWith(".md"));
+    return markdownFiles;
+  }
+
+  async function parseProjects(): Promise<Project[]> {
+    const projects: Project[] = [];
+
+    for (const file of markdownFiles) {
+      const filePath = path.join(directory, file);
+      const fileContent = await fs.readFile(filePath, "utf-8");
+      const parsed = matter(fileContent) as {
+        attributes: { name: string; tools: string[] };
+        body: string;
+      };
+      projects.push({
+        name: parsed.attributes.name,
+        description: await marked(parsed.body),
+        tools: parsed.attributes.tools,
+      });
+    }
+
+    return projects;
+  }
+}
